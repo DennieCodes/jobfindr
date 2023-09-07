142e6adf1347aa45e55519ec54a32f8df606e889
Dennie <oculareo@gmail.com>
2023-09-07 13:53:50 -0400

    Added base application model

--- applications/models.py
+++ applications/models.py
@@ -4,7 +4,10 @@ class Application(models.Model):
   company = models.CharField(max_length=200)
   link = models.URLField(max_length=256)
   role = models.CharField(max_length=200)
-  type_of_position = models.CharField(max_length=50)
   description = models.TextField()
-  type_of_application = models.CharField(max_length=75)
-  response = models.CharField(max_length=100)
+
+  def __str__(self):
+    return self.name
+
+  class Meta:
+    ordering = ["company"]
