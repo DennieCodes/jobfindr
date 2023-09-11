5b5b849aa8fb4435c034d836d40f664ac9a38031
Dennie <oculareo@gmail.com>
2023-09-11 19:42:24 -0400

    Added home page and set default url to home

--- jobfindr/urls.py
+++ jobfindr/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def redirect_to_home(request):
+  return redirect("home")
 
 urlpatterns = [
+    path('', redirect_to_home, name="home"),
+    path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('applications/', include('applications.urls'))
