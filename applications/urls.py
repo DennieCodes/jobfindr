393122e8beb6870ad3082b313dc463ef36e478ae
Dennie <oculareo@gmail.com>
2023-09-14 08:55:40 -0400

    Added delete functionality for applications

--- applications/urls.py
+++ applications/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from applications.views import list_applications, update_application
+from applications.views import list_applications, update_application, delete_application
 
 urlpatterns = [
   path("", list_applications, name="list_applications"),
   path("<int:id>/update/", update_application, name="update_application"),
+  path("<int:id>/delete/", delete_application, name="delete_application"),
 ]
